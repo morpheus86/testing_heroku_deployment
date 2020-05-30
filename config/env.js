8b102f1f46056865d41c39f06ff62f55e23f8f7a
hamadou diallo <hamadou_diallo@yahoo.com>
2020-05-30 15:44:51 -0400

    updated commit

---
+++ config/env.js
@@ -0,0 +1,16 @@
+const env = {
+  database: "d8e793itnasopn",
+  username: "lhlsnoniavodbs",
+  password: "288f7353001cd6aae0817afafcc224ef2b147acbf6ed8d849f7c18cb10e0d799",
+  host: "ec2-18-209-187-54.compute-1.amazonaws.com",
+  port: 5432,
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+};
+
+module.exports = env;
--- server.js
+++ server.js
@@ -1,8 +1,44 @@
-const express = require("express");
-const app = express();
-const morgan = require("morgan");
-const body_parser = require("body-parser");
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("Hello World"));
-const port = process.env.PORT || 3500;
-app.listen(port, () => console.log("Listening on Port ", port));
+const db = require("./config/db.config");
+
+const Customer = db.customers;
+
+// force: true will drop the table if it already exists
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and Resync with { force: true }");
+  initial();
+});
+
+require("./app/route/customer.route.js")(app);
+
+// Create a Server
+var server = app.listen(process.env.PORT || 5000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("App listening at http://%s:%s", host, port);
+});
+
+function initial() {
+  Customer.create({
+    firstname: "Jack",
+    lastname: "Davis",
+    age: 25,
+  });
+
+  Customer.create({
+    firstname: "Mary",
+    lastname: "Taylor",
+    age: 37,
+  });
+
+  Customer.create({
+    firstname: "Peter",
+    lastname: "Smith",
+    age: 32,
+  });
+}
